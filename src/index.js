924eea87fad36efde734a4dfbd078f54be6e9b5d
Pradel Léo <pradel.leo@gmail.com>
2016-12-27 11:19:01 +0100

    Start createUser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
+import _ from 'lodash';
+
+// TODO setup all mongodb indexes
+
 class Mongo {
-  constructor(options) {
+  constructor(db, options) {
     const defaultOptions = {
       collectionName: 'users',
+      timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+      },
     };
     this.options = Object.assign({}, defaultOptions, options);
+    if (_.get(db, 'constructor.define.name') !== 'Db') {
+      throw new Error('A valid database connetion object is required');
+    }
+    this.db = db;
+    this.collection = this.db.collection(this.options.collectionName);
   }
 
-  createUser() { // eslint-disable-line class-methods-use-this
-    return Promise.resolve('user');
+  createUser(options) {
+    const user = {
+      services: {},
+      [this.options.timestamps.createdAt]: new Date(),
+    };
+    // TODO hash password
+    if (options.password) {
+      user.services.password = { bcrypt: options.password };
+    }
+    if (options.username) {
+      user.username = options.username;
+    }
+    if (options.email) {
+      user.emails = [{ address: options.email, verified: false }];
+    }
+    return this.collection.insert(user).then((data) => {
+      if (data.ops) {
+        return data.ops[0];
+      }
+      return data;
+    });
+  }
+
+  findUserByEmail(email) {
+    return this.collection.findOne({ 'emails.address': email });
+  }
+
+  findUserByUsername(username) {
+    return this.collection.findOne({ username });
   }
 }
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,27 +1,95 @@
+import mongodb from 'mongodb';
 import Mongo from './index';
 
+let db;
+const user = {
+  username: 'johndoe',
+  email: 'john@doe.com',
+  password: 'toto',
+  profile: {},
+};
+
+function createConnection(cb) {
+  const url = 'mongodb://localhost:27017/accounts-mongo-tests';
+  mongodb.MongoClient.connect(url, (err, dbArg) => {
+    db = dbArg;
+    cb(err);
+  });
+}
+
+function closeConnection(cb) {
+  db.dropDatabase((err) => {
+    if (err) cb(err);
+    db.close();
+    cb();
+  });
+}
+
 describe('Mongo', () => {
+  beforeAll(createConnection);
+
   describe('#constructor', () => {
     it('should have default options', () => {
-      const mongo = new Mongo();
+      const mongo = new Mongo(db);
       expect(mongo.options).toBeTruthy();
     });
 
     it('should overwrite options', () => {
-      const mongo = new Mongo({
+      const mongo = new Mongo(db, {
         collectionName: 'users-test',
       });
       expect(mongo.options).toBeTruthy();
       expect(mongo.options.collectionName).toEqual('users-test');
     });
+
+    it('should throw with an invalid database connection object', () => {
+      try {
+        new Mongo(); // eslint-disable-line no-new
+        throw new Error();
+      } catch (err) {
+        expect(err.message).toBe('A valid database connetion object is required');
+      }
+    });
   });
 
   describe('createUser', () => {
-    const mongo = new Mongo();
-
-    it('should resolve promise', async () => {
-      const ret = await mongo.createUser();
-      expect(ret).toEqual('user');
+    it('should create a new user', async () => {
+      const mongo = new Mongo(db);
+      const ret = await mongo.createUser(user);
+      expect(ret._id).toBeTruthy(); // eslint-disable-line no-underscore-dangle
+      expect(ret.emails[0].address).toBe(user.email);
+      expect(ret.emails[0].verified).toBe(false);
+      expect(ret.createdAt).toBeTruthy();
     });
   });
+
+  describe('findUserByEmail', () => {
+    it('should return null for not found user', async () => {
+      const mongo = new Mongo(db);
+      const ret = await mongo.findUserByEmail('unknow@user.com');
+      expect(ret).not.toBeTruthy();
+    });
+
+    it('should return user', async () => {
+      const mongo = new Mongo(db);
+      const ret = await mongo.findUserByEmail(user.email);
+      expect(ret).toBeTruthy();
+    });
+  });
+
+  describe('findUserByUsername', () => {
+    it('should return null for not found user', async () => {
+      const mongo = new Mongo(db);
+      const ret = await mongo.findUserByUsername('unknowuser');
+      expect(ret).not.toBeTruthy();
+    });
+
+    it('should return user', async () => {
+      const mongo = new Mongo(db);
+      const ret = await mongo.findUserByUsername(user.username);
+      expect(ret).toBeTruthy();
+    });
+  });
+
+  afterAll(closeConnection);
 });
